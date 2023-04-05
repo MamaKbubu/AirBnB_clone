161454b9327ea1101e9f20401f8ea12f9ead4bc0
MamaKbubu <maboelatk@gmail.com>
2023-04-05 20:38:56 +0200

    Git merge

--- models/file_storage.py
+++ models/file_storage.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""This code will control the storage of the file or rather the website"""
+"""This code will control the storage of the file or 
+rather the website"""
 
 import json
 import os
@@ -27,17 +28,16 @@ def reload(self):
         with open(self.__file_path, "r") as f:
             serialized_objs = json.load(f)
             for key, serialized_obj in serialized_objs.items():
-                    class_name, obj_id = key.split(".")
-                    obj_dict = {}
-                    for attr, value in serialized_obj.items():
-                        if attr == "__class__":
-                            class_name = value
-                        else:
-                            obj_dict[attr] = value
-                    module = __import__("models." + class_name.lower(), fromlist=[class_name])
-                    cls = getattr(module, class_name)
-                    obj = cls(**obj_dict)
-                    self.__objects[key] = obj
-        except FileNotFoundError:
-            pass
- 
+                class_name, obj_id = key.split(".")
+                obj_dict = {}
+                for attr, value in serialized_obj.items():
+                    if attr == "__class__":
+                        class_name = value
+                    else:
+                        obj_dict[attr] = value
+                module = __import__("models." + class_name.lower(), fromlist=[class_name])
+                cls = getattr(module, class_name)
+                obj = cls(**obj_dict)
+                self.__objects[key] = obj
+    except FileNotFoundError:
+        pass
